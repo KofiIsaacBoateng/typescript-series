0e72e4ee3fdccff6794d9832aec5685e255e1cf3
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-09 20:17:18 +0000

    add: compiled 04-arrays.ts

---
+++ 04-arrays/04-arrays.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// common syntax 
+var superHeroes = ["Batman", "Aquaman", "Superman"]; // array of strings
+var ranks = [7, 5, 10];
+var hasWeakness = [false, false, true];
+//alternative syntax
+var superHeroesAlt = superHeroes;
+var ranksAlt = ranks;
+var hasWeaknessAlt = hasWeakness;
+var superVillains; // common
+var superVillainsAlt; //alt
+superVillains = [{
+        name: 'Every woman',
+        rank: 11,
+        hasWeakness: false,
+        superPower: "numb men"
+    }];
+// array of arrays **2D**
+var coordinates;
+coordinates = [
+    [1.120, 7.282],
+    [1.092, 6.322]
+];
