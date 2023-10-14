5c4fabf99136ed506946e0af99a255d9417bdb51
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-14 10:10:01 +0000

    add: interface update

--- 08-interface/08-interface.ts
+++ 08-interface/08-interface.ts
@@ -1,8 +1,22 @@
+// basic interface
 interface User {
-     readonly _id: string | boolean
+     readonly _id: string | number
      name: string,
      email: string,
      isVerified: boolean,
      role: "Tutor" | "Student" | "University",
      isActive?: boolean | string
+}
+
+const getUser = (_id: number | string ): User => {
+     if (!_id) throw new Error("Please provide user's id")
+
+     return {
+          _id,
+          name: "James Bran",
+          email: "bran@gmail.com",
+          isVerified: true,
+          role: 'Student',
+          isActive: "false"
+     }
 }
